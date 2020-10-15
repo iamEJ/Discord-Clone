25c47da55335e3365f64c70899c72309e0a2bf59
iamEJ <jukneviciute185@gmail.com>
2020-10-15 15:11:42 +0300

    add and style chat header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Sidebar from "./Components/Sidebar";
+import Chat from "./Components/Chat";
 
 function App() {
   return (
     <div className="app">
       <Sidebar />
+      <Chat />
     </div>
   );
 }
